b7194cfbc3210a6b557c1590f6a221696ec5ff2b
Stephen Turley <stephen@turley.dev>
2021-04-03 13:47:53 -0400

    added command parser with doctests

---
+++ kv_umbrella/apps/kv_server/lib/kv_server/command.ex
@@ -0,0 +1,39 @@
+defmodule KVServer.Command do
+  @doc ~S"""
+  Parses given `line` into a command.
+
+  ## Examples
+
+      iex> KVServer.Command.parse("CREATE shopping\r\n")
+      {:ok, {:create, "shopping"}}
+
+      iex> KVServer.Command.parse("CREATE  shopping  \r\n")
+      {:ok, {:create, "shopping"}}
+
+      iex> KVServer.Command.parse("PUT shopping milk 1\r\n")
+      {:ok, {:put, "shopping", "milk", "1"}}
+
+      iex> KVServer.Command.parse("GET shopping milk\r\n")
+      {:ok, {:get, "shopping", "milk"}}
+
+      iex> KVServer.Command.parse("DELETE shopping eggs\r\n")
+      {:ok, {:delete, "shopping", "eggs"}}
+
+  Unknown commands or commmands with the wrong number of arguments return an error:
+      
+      iex> KVServer.Command.parse("UNKNOWN shopping eggs\r\n")
+      {:error, :unknown_command}
+
+      iex> KVServer.Command.parse("GET shopping\r\n")
+      {:error, :unknown_command}
+  """
+  def parse(line) do
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["GET", bucket, item] -> {:ok, {:get, bucket, item}}
+      ["DELETE", bucket, item] -> {:ok, {:delete, bucket, item}}
+      ["PUT", bucket, item, value] -> {:ok, {:put, bucket, item, value}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+end
